513f8b76fe1f5635380fd22e6b77358d3231a517
alok1929 <alokhegde221@gmail.com>
2022-08-10 03:48:30 +0530

    ready for netlify

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    colors:{
+      VeryDarkMagenta: "hsl(300, 43%, 22%)",
+      SoftPink: "hsl(333, 80%, 67%)",
+      DarkGrayishMagenta: "hsl(303, 10%, 53%)",
+      LightGrayishMagenta: "hsl(300, 24%, 96%)",
+      White: "hsl(0, 0%, 100%)",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
